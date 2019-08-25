bb98ecda1c89315ac73aaaa91f474b9f630f7154
Aeronica <pmboese@gmail.com>
2019-08-25 15:45:09 -0500

    Split MML tokens/element out to eliminate redundant case checks.

--- src/main/java/net/aeronica/libs/mml/parser/ElementTypes.java
+++ src/main/java/net/aeronica/libs/mml/parser/ElementTypes.java
@@ -4,17 +4,24 @@ package net.aeronica.libs.mml.parser;
  */
 public class ElementTypes
 {
-    public static final byte MML_CMD = 1;       // [iopstvIOPSTV] Instrument, Octave, Perform, Sustain, Tempo, Volume
-    public static final byte MML_LEN = 2;       // [lL] Length Command
-    public static final byte MML_OCT = 3;       // [<>] Octave down/up
-    public static final byte MML_NOTE = 4;      // [a-gA-G] Notes
-    public static final byte MML_ACC = 5;       // [+#-] Accidental
-    public static final byte MML_MIDI = 6;      // [nN] MIDI note
-    public static final byte MML_DOT = 7;       // '.' dotted
-    public static final byte MML_TIE = 8;       // '&' Tie
-    public static final byte MML_REST = 9;      // [rR] Rest
-    public static final byte MML_NUMBER = 10;      // Positive Integer
-    public static final byte MML_BEGIN = 11;    // 'MML@' MML Begin PART/Instrument
-    public static final byte MML_CHORD = 12;    // ',' Add Chord
-    public static final byte MML_END = 13;      // ';' MML End PART/Instrument
+    public static final byte MML_INSTRUMENT = 1;    // [iI]<0-99999> Instrument Command in packed preset format
+    public static final byte MML_LENGTH = 2;        // [lL] Length Command
+    public static final byte MML_OCTAVE = 3;        // [oO]<1-8> Octave Command
+    public static final byte MML_PERFORM = 4;       // [pP]<1-5> Perform Command
+    public static final byte MML_SUSTAIN = 5;       // [sS]<0-1> Sustain Command
+    public static final byte MML_TEMPO = 6;         // [tT]<32-255> Tempo Command
+    public static final byte MML_VOLUME = 7;        // [vV]<1-15 | 0-127> Volume Command in "Mabinogi/Maple Story 2" or ArcheAge levels
+    public static final byte MML_OCTAVE_UP = 8;     // [>] Octave Up Command
+    public static final byte MML_OCTAVE_DOWN = 9;   // [<] Octave Down Command
+    public static final byte MML_NOTE = 10;         // [a-gA-G] Notes
+    public static final byte MML_SHARP = 11;        // [+#] Accidental Sharp
+    public static final byte MML_FLAT = 12;         // [-] Accidental Flat
+    public static final byte MML_MIDI = 13;         // [nN] MIDI note
+    public static final byte MML_DOT = 14;          // '.' dotted
+    public static final byte MML_TIE = 15;          // '&' Tie
+    public static final byte MML_REST = 16;         // [rR] Rest
+    public static final byte MML_NUMBER = 17;       // <0-99999> Positive Integer
+    public static final byte MML_BEGIN = 18;        // 'MML@' MML Begin
+    public static final byte MML_CHORD = 19;        // ',' Add Chord/Harmony/Counterpoint
+    public static final byte MML_END = 20;          // ';' MML End
 }
--- src/main/java/net/aeronica/libs/mml/parser/MMLParser.java
+++ src/main/java/net/aeronica/libs/mml/parser/MMLParser.java
@@ -3,7 +3,7 @@ package net.aeronica.libs.mml.parser;
 import net.aeronica.libs.mml.core.DataCharBuffer;
 import net.aeronica.libs.mml.core.IndexBuffer;
 
-public class MMLParser2
+public class MMLParser
 {
     private int    position     = 0;
     private int    elementIndex = 0;
@@ -19,23 +19,22 @@ public class MMLParser2
             switch (buffer.data[position])
             {
                 case 'i':
-                case 'o':
+                case 'I': { setElementDataLength(elementBuffer, elementIndex++, ElementTypes.MML_INSTRUMENT, this.position); } break;
                 case 'p':
+                case 'P': { setElementDataLength(elementBuffer, elementIndex++, ElementTypes.MML_PERFORM, this.position); } break;
                 case 's':
+                case 'S': { setElementDataLength(elementBuffer, elementIndex++, ElementTypes.MML_SUSTAIN, this.position); } break;
                 case 't':
+                case 'T': { setElementDataLength(elementBuffer, elementIndex++, ElementTypes.MML_TEMPO, this.position); } break;
+                case 'o':
+                case 'O': { setElementDataLength(elementBuffer, elementIndex++, ElementTypes.MML_OCTAVE, this.position); } break;
                 case 'v':
-                case 'I':
-                case 'O':
-                case 'S':
-                case 'P':
-                case 'T':
-                case 'V': { setElementDataLength1(elementBuffer, elementIndex++, ElementTypes.MML_CMD, this.position); } break;
-
+                case 'V': { setElementDataLength(elementBuffer, elementIndex++, ElementTypes.MML_VOLUME, this.position); } break;
                 case 'l':
-                case 'L': { setElementDataLength1(elementBuffer, elementIndex++, ElementTypes.MML_LEN, this.position); } break;
+                case 'L': { setElementDataLength(elementBuffer, elementIndex++, ElementTypes.MML_LENGTH, this.position); } break;
 
-                case '<':
-                case '>': { setElementDataLength1(elementBuffer, elementIndex++, ElementTypes.MML_OCT, this.position); } break;
+                case '>': { setElementDataLength(elementBuffer, elementIndex++, ElementTypes.MML_OCTAVE_UP, this.position); } break;
+                case '<': { setElementDataLength(elementBuffer, elementIndex++, ElementTypes.MML_OCTAVE_DOWN, this.position); } break;
 
                 case 'a':
                 case 'b':
@@ -43,27 +42,28 @@ public class MMLParser2
                 case 'd':
                 case 'e':
                 case 'f':
+                case 'g':
                 case 'A':
                 case 'B':
                 case 'C':
                 case 'D':
                 case 'E':
                 case 'F':
-                case 'G': { setElementDataLength1(elementBuffer, elementIndex++, ElementTypes.MML_NOTE, this.position); } break;
+                case 'G': { setElementDataLength(elementBuffer, elementIndex++, ElementTypes.MML_NOTE, this.position); } break;
 
                 case '+':
-                case '#':
-                case '-': { setElementDataLength1(elementBuffer, elementIndex++, ElementTypes.MML_ACC, this.position); } break;
+                case '#': { setElementDataLength(elementBuffer, elementIndex++, ElementTypes.MML_SHARP, this.position); } break;
+                case '-': { setElementDataLength(elementBuffer, elementIndex++, ElementTypes.MML_FLAT, this.position); } break;
 
                 case 'n':
-                case 'N': { setElementDataLength1(elementBuffer, elementIndex++, ElementTypes.MML_MIDI, this.position); } break;
+                case 'N': { setElementDataLength(elementBuffer, elementIndex++, ElementTypes.MML_MIDI, this.position); } break;
 
-                case '.': { setElementDataLength1(elementBuffer, elementIndex++, ElementTypes.MML_DOT, this.position); } break;
+                case '.': { setElementDataLength(elementBuffer, elementIndex++, ElementTypes.MML_DOT, this.position); } break;
 
-                case '&': { setElementDataLength1(elementBuffer, elementIndex++, ElementTypes.MML_TIE, this.position); } break;
+                case '&': { setElementDataLength(elementBuffer, elementIndex++, ElementTypes.MML_TIE, this.position); } break;
 
                 case 'r':
-                case 'R': { setElementDataLength1(elementBuffer, elementIndex++, ElementTypes.MML_REST, this.position); } break;
+                case 'R': { setElementDataLength(elementBuffer, elementIndex++, ElementTypes.MML_REST, this.position); } break;
 
                 case '0':
                 case '1':
@@ -77,8 +77,8 @@ public class MMLParser2
                 case '9': { parseNumberToken(buffer, elementBuffer); elementIndex++; } break;
 
                 case 'M': { parseMMLBegin(buffer, elementBuffer); elementIndex++;  break;}
-                case ',': { setElementDataLength1(elementBuffer, elementIndex++, ElementTypes.MML_CHORD, this.position); } break;
-                case ';': { setElementDataLength1(elementBuffer, elementIndex++, ElementTypes.MML_END, this.position); } break;
+                case ',': { setElementDataLength(elementBuffer, elementIndex++, ElementTypes.MML_CHORD, this.position); } break;
+                case ';': { setElementDataLength(elementBuffer, elementIndex++, ElementTypes.MML_END, this.position); } break;
             }
         }
         elementBuffer.count = this.elementIndex;
@@ -120,7 +120,7 @@ public class MMLParser2
         this.position = tempPos -1; // -1 because the outer for-loop adds 1 to the position too
     }
 
-    private void setElementDataLength1(IndexBuffer elementBuffer, int index, byte type, int position) {
+    private void setElementDataLength(IndexBuffer elementBuffer, int index, byte type, int position) {
         elementBuffer.type    [index] = type;
         elementBuffer.position[index] = position;
         elementBuffer.length  [index] = 1;
