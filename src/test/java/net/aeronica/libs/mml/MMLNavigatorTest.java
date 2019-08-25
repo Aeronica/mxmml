aad35f31164e384112a03f08d1837fe242744a83
Aeronica <pmboese@gmail.com>
2019-08-24 21:08:46 -0500

    Run both tests

--- src/test/java/net/aeronica/libs/mml/MMLNavigatorTest.java
+++ src/test/java/net/aeronica/libs/mml/MMLNavigatorTest.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.assertTrue;
 public class MMLNavigatorTest
 {
 
+    @Test
     public void testWithParser1()
     {
         DataCharBuffer dataBuffer = new DataCharBuffer();
