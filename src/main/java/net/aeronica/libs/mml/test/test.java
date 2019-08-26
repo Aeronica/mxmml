6641fa3422f594852ba213e085ae599417579929
Aeronica <pmboese@gmail.com>
2019-08-25 21:52:50 -0500

    Tinker with the navigator.

---
+++ src/main/java/net/aeronica/libs/mml/test/test.java
@@ -0,0 +1,55 @@
+package net.aeronica.libs.mml.test;
+
+import net.aeronica.libs.mml.core.DataCharBuffer;
+import net.aeronica.libs.mml.core.IndexBuffer;
+import net.aeronica.libs.mml.oldcore.MMLUtil;
+import net.aeronica.libs.mml.oldcore.TestData;
+import net.aeronica.libs.mml.parser.ElementTypes;
+import net.aeronica.libs.mml.parser.MMLNavigator;
+import net.aeronica.libs.mml.parser.MMLParser;
+
+public class test
+{
+    private static String mmlString = TestData.MML2.getMML();
+
+    public static void main(String[] args) throws Exception
+    {
+        DataCharBuffer dataBuffer = new DataCharBuffer();
+        dataBuffer.data = mmlString.toCharArray();
+        dataBuffer.length = dataBuffer.data.length;
+
+        IndexBuffer elementBuffer = new IndexBuffer(dataBuffer.data.length, true);
+
+        MMLParser parser = new MMLParser();
+        parser.parse(dataBuffer, elementBuffer);
+
+        MMLNavigator navigator = new MMLNavigator(dataBuffer, elementBuffer);
+        if (!navigator.hasNext()) return;
+        for (int i = 0; i < elementBuffer.count; i++)
+        {
+            switch(navigator.type())
+            {
+                case ElementTypes.MML_INSTRUMENT: { MMLUtil.MML_LOGGER.info(" instrument"); } break;
+                case ElementTypes.MML_OCTAVE: { MMLUtil.MML_LOGGER.info(" octave"); } break;
+                case ElementTypes.MML_PERFORM: { MMLUtil.MML_LOGGER.info(" perform"); } break;
+                case ElementTypes.MML_SUSTAIN: { MMLUtil.MML_LOGGER.info(" sustain"); } break;
+                case ElementTypes.MML_TEMPO: { MMLUtil.MML_LOGGER.info(" tempo"); } break;
+                case ElementTypes.MML_VOLUME: { MMLUtil.MML_LOGGER.info(" volume"); } break;
+                case ElementTypes.MML_OCTAVE_UP: { MMLUtil.MML_LOGGER.info("  >"); } break;
+                case ElementTypes.MML_OCTAVE_DOWN: { MMLUtil.MML_LOGGER.info("  <"); } break;
+                case ElementTypes.MML_NOTE: { MMLUtil.MML_LOGGER.info(" note: {}", navigator.asChar()); } break;
+                case ElementTypes.MML_SHARP: { MMLUtil.MML_LOGGER.info("  +"); } break;
+                case ElementTypes.MML_FLAT: { MMLUtil.MML_LOGGER.info("  -"); } break;
+                case ElementTypes.MML_MIDI: { MMLUtil.MML_LOGGER.info(" midi"); } break;
+                case ElementTypes.MML_DOT: { MMLUtil.MML_LOGGER.info("  ."); } break;
+                case ElementTypes.MML_TIE: { MMLUtil.MML_LOGGER.info("  &"); } break;
+                case ElementTypes.MML_REST: { MMLUtil.MML_LOGGER.info(" rest"); } break;
+                case ElementTypes.MML_NUMBER: { MMLUtil.MML_LOGGER.info("  {}", navigator.asInt()); } break;
+                case ElementTypes.MML_BEGIN: { MMLUtil.MML_LOGGER.info("BEGIN"); } break;
+                case ElementTypes.MML_CHORD: { MMLUtil.MML_LOGGER.info("CHORD"); } break;
+                case ElementTypes.MML_END: { MMLUtil.MML_LOGGER.info("END"); } break;
+            }
+            navigator.next();
+        }
+    }
+}
